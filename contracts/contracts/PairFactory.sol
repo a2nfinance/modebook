e5ef18528c9cfcfce33748f228a39057396af719
a2nfinance <levi@a2n.finance>
2023-12-17 19:08:14 +0700

    Add mock ERC-20 contracts and PairFactory contract

---
+++ contracts/contracts/PairFactory.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IPairFactory.sol";
+import "./interfaces/ISFS.sol";
+import "./Pair.sol";
+import "./SFSRegister.sol";
+
+contract PairFactory is IPairFactory, ISFS, Ownable {
+  address public sfsAddress;
+  address public feeReceiver;
+  uint16 public feeRate;
+  mapping(address => mapping(address => address)) public pairMap;
+  address[] public allPairs;
+
+  constructor(address _sfsAddress, address _admin, address _receiver, uint16 _feeRate) {
+    sfsAddress = _sfsAddress;
+    feeRate = _feeRate;
+    feeReceiver = _receiver;
+    SFSRegister sfsContract = SFSRegister(_sfsAddress);
+    sfsContract.register(_receiver);
+    _transferOwnership(_admin);
+  }
+
+  function changeReceiver(address _newReceiver) external override onlyOwner returns (bool) {
+    require(feeReceiver != _newReceiver, "feeReceiver==_newReceiver");
+    require(_newReceiver != address(0), "address(0)");
+    feeReceiver = _newReceiver;
+    SFSRegister sfsContract = SFSRegister(sfsAddress);
+    sfsContract.register(_newReceiver);
+    return true;
+  }
+
+  function changeSFSAddress(address _newSFSAddress) external override onlyOwner returns (bool) {
+    require(sfsAddress != _newSFSAddress, "sfsAddress==_newSFSAddress");
+    require(_newSFSAddress != address(0), "address(0)");
+    sfsAddress = _newSFSAddress;
+    SFSRegister sfsContract = SFSRegister(_newSFSAddress);
+    sfsContract.register(feeReceiver);
+    return true;
+  }
+
+  function changeFeeRate(uint16 _feeRate) external override onlyOwner returns (bool) {
+    require(feeRate != _feeRate, "feeRate==_feeRate");
+    feeRate = _feeRate;
+    return true;
+  }
+
+  function createPair(address _tokenA, address _tokenB) external override onlyOwner returns (address pair) {
+    require(_tokenA != _tokenB, "tokenA==tokenB");
+    require(_tokenA != address(0), "TokenA:0x0");
+    require(pairMap[_tokenA][_tokenB] == address(0), "Existed");
+
+    pair = address(new Pair(_tokenA, _tokenB, owner(), sfsAddress, feeReceiver, feeRate));
+
+    pairMap[_tokenA][_tokenB] = pair;
+    pairMap[_tokenB][_tokenA] = pair;
+    allPairs.push(pair);
+
+    emit CreatePair(pair);
+    return pair;
+  }
+
+  function countPairs() external view override returns (uint256) {
+    return allPairs.length;
+  }
+
+  // Check Owner
+  function removePair(uint256 _index) external override onlyOwner returns (uint256) {}
+}
---
+++ contracts/contracts/mocks/WETH.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract WETH is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 1000000 * (10**decimals()));
+    }
+}
